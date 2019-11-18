b30634c01fd65cf363840fdc45e668f2a00f353d
Kevin Risden <krisden@apache.org>
2019-11-17 21:06:31 -0500

    Initial okhttp client test

--- src/test/java/io/github/risdenk/experiments/http/HttpExperimentsTest.java
+++ src/test/java/io/github/risdenk/experiments/http/HttpExperimentsTest.java
@@ -1,5 +1,8 @@
 package io.github.risdenk.experiments.http;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -68,6 +71,15 @@ class HttpExperimentsTest {
       try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
         Assertions.assertEquals(HttpStatus.OK_200, response.getStatusLine().getStatusCode());
       }
+    }
+  }
+
+  @Test
+  void testOkHttpClientGet() throws Exception {
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder().url(serverUri.toString()).build();
+    try (Response response = client.newCall(request).execute()) {
+      Assertions.assertEquals(HttpStatus.OK_200, response.code());
     }
   }
 }
