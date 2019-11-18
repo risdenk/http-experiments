8436bcf8f82dc70834d016e44db06c538aff0fbf
Kevin Risden <krisden@apache.org>
2019-11-17 20:57:06 -0500

    Initial Jetty Test

--- src/test/java/io/github/risdenk/experiments/http/HttpExperimentsTest.java
+++ src/test/java/io/github/risdenk/experiments/http/HttpExperimentsTest.java
@@ -1,11 +1,59 @@
 package io.github.risdenk.experiments.http;
 
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.net.HttpURLConnection;
+import java.net.URI;
+
 class HttpExperimentsTest {
+  private static Server server;
+  private static URI serverUri;
+
+  @BeforeAll
+  static void startJetty() throws Exception {
+    server = new Server();
+    ServerConnector connector = new ServerConnector(server);
+    connector.setPort(0); // auto-bind to available port
+    server.addConnector(connector);
+
+    ServletContextHandler context = new ServletContextHandler();
+    ServletHolder defaultServ = new ServletHolder("default", DefaultServlet.class);
+    defaultServ.setInitParameter("resourceBase", System.getProperty("user.dir"));
+    defaultServ.setInitParameter("dirAllowed", "true");
+    context.addServlet(defaultServ, "/");
+    server.setHandler(context);
+
+    // Start Server
+    server.start();
+
+    // Determine Base URI for Server
+    String host = connector.getHost();
+    if (host == null)
+    {
+      host = "localhost";
+    }
+    int port = connector.getLocalPort();
+    serverUri = new URI(String.format("http://%s:%d/", host, port));
+  }
+
+  @AfterAll
+  static void stopJetty() throws Exception {
+    server.stop();
+  }
+
   @Test
-  void testAdd() {
-    Assertions.assertEquals(42, Integer.sum(19, 23));
+  void testGet() throws Exception {
+    HttpURLConnection http = (HttpURLConnection) serverUri.resolve("/").toURL().openConnection();
+    http.connect();
+    Assertions.assertEquals(HttpStatus.OK_200, http.getResponseCode());
   }
 }
